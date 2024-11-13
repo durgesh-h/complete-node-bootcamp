a27bd027e0027f93b2013b88f0c9b3e6442dc5b6
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-11-13 19:47:38 +0530

    1.2routing done

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -1,12 +1,30 @@
 const fs= require('fs');
 const http = require('http');
+const { type } = require('os');
+const url = require('url');
 
 
 /////////////////////// Server created successfully!! //////////////////////////
 
 const server = http.createServer((req, res)=>{
-    res.end('Hello from the server!');
-    console.log(res)
+    pathName = req.url;
+
+    if (pathName=== '/'|| pathName === '/overview'){
+        res.end(
+            'This is the OVERVIEW page.'
+        )
+    }
+    else if( pathName==='/product'){
+        res.end('This is the PRODUCT page.')
+    }
+    else{
+        res.writeHead(404, {
+         'Content-type': 'text/html',
+         'my-own-header': 'hello-shonna'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+    
 });
 
 server.listen(8000, '127.0.0.1', ()=>{
@@ -15,3 +33,5 @@ server.listen(8000, '127.0.0.1', ()=>{
 })
 
 ////////////////////////////////////////////////////////////////////////////////
+
+
