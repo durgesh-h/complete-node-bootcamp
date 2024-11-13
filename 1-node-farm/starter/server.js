0e047c302cae93c994e29f7dd7666140654a4b74
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-11-13 23:54:40 +0530

    1.5filling the templates

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -3,17 +3,43 @@ const http = require("http");
 const url = require("url");
 
 /////////////////////// Server created successfully!! //////////////////////////
+const replaceTemplate = (temp, product)=>{
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+   output = output.replace(/{%IMAGE%}/g, product.image);
+   output = output.replace(/{%PRICE%}/g, product.price);
+   output = output.replace(/{%FROM%}/g, product.from);
+   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+   output = output.replace(/{%ID%}/g, product.id);
+   if(!product.organic) output= output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+   return output;
+}
 
-data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const Dataobj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   pathName = req.url;
-
+  //OVERVIEW PAGE
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW page.");
-  } else if (pathName === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const cardsHtml = Dataobj.map(el=>replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    res.end(output);
+  }
+  //PRODUCT PAGE
+  else if (pathName === "/product") {
     res.end("This is the PRODUCT page.");
-  } else if (pathName === "/api") {
+  }
+  //NOT FOUND PAGE
+  else if (pathName === "/api") {
     res.end(data);
   } else {
     res.writeHead(404, {
