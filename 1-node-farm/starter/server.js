57980f627c1cc1d1ec8a0e0c4f78212c9fe65841
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-11-13 19:13:10 +0530

    1.1Sever created successfully

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -1,6 +1,9 @@
 const fs= require('fs');
 const http = require('http');
 
+
+/////////////////////// Server created successfully!! //////////////////////////
+
 const server = http.createServer((req, res)=>{
     res.end('Hello from the server!');
     console.log(res)
@@ -10,3 +13,5 @@ server.listen(8000, '127.0.0.1', ()=>{
     console.log('listening to requests on port 8000');
   
 })
+
+////////////////////////////////////////////////////////////////////////////////
